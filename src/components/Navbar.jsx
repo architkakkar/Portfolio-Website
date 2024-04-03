72a82491e06d3cd136d9e7b27fa2a24326d837d1
Archit Kakkar <education.archit@gmail.com>
2024-04-04 03:14:13 +0530

    Feature: Add responsive sidebar for mobile navigation.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,12 +17,12 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between">
       <aside
-        className={`absolute top-0 left-0 flex flex-col w-3/5 h-screen p-5 transition-all duration-500 ease-in-out border-r border-r-light bg-[#121212] gap-y-5 text-xl rounded-lg ${
+        className={`absolute top-0 left-0 flex flex-col w-3/5 h-screen p-5 transition-all duration-500 ease-in-out border-r-2 dark:border-r-light border-r-dark bg-[#ecf4dc] dark:bg-[#121212] gap-y-5 text-xl rounded-r-lg ${
           isSidebar ? "translate-x-0" : "-translate-x-full"
         }`}
       >
         <button
-          className="px-1.5 py-1 text-sm text-[#f87171] bg-[#f871711a] rounded-md w-fit mb-4"
+          className="px-1.5 py-1 text-sm dark:text-[#f87171] dark:bg-[#f871711a] text-[#f05b5b] bg-[#f85e5e38] rounded-md w-fit mb-4"
           onClick={hideSidebar}
         >
           Close
