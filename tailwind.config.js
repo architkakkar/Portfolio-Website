836dde7d876451fb0cb63bb6ea35b1821675c3a4
Archit Kakkar <education.archit@gmail.com>
2024-04-04 02:49:18 +0530

    Add custom styles to Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // This enables class-based dark mode
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        wendy: "Wendy One, sans-serif",
+      },
+      colors: {
+        dark: "#0E0E0E",
+        light: "#ECF4D6",
+      },
+      backgroundColor: {
+        dark: "#0E0E0E",
+        light: "#ECF4D6",
+        darkglass: "#1e1e1ef0",
+      },
+    },
   },
   plugins: [],
 };
