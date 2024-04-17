41bd30b13310ec2b4864ce102bbee636854ca8e4
Archit Kakkar <education.archit@gmail.com>
2024-04-18 03:32:42 +0530

    Upgrade to handle DarkMode with useContext()

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import DarkModeContext from "./components/DarkModeContext";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -7,8 +8,25 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState();
   const [isSidebar, setIsSidebar] = useState(false);
   const [page, setPage] = useState("projects");
+
+  // Ensure dark mode is applied on initial load if the user has it enabled in their system settings
+  useEffect(() => {
+    setIsDarkMode(() => {
+      const darkMode =
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+      if (darkMode) {
+        document.documentElement.classList.add("dark");
+        return true;
+      }
+
+      return false;
+    });
+  }, []);
 
   function showSidebar() {
     setIsSidebar(true);
@@ -18,29 +36,31 @@ function App() {
   }
 
   return (
-    <div
-      className={`w-screen h-full p-5 pb-4 font-wendy dark:bg-dark text-dark bg-light dark:text-light ${
-        isSidebar ? "h-screen overflow-hidden" : ""
-      }`}
-    >
-      <Header
-        isSidebar={isSidebar}
-        showSidebar={showSidebar}
-        hideSidebar={hideSidebar}
-        // setPage={setPage}
-      />
-      {isSidebar && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-80"
-          onClick={hideSidebar}
-        ></div>
-      )}
-      {page === "home" && <Home />}
-      {page === "about" && <About />}
-      {page === "projects" && <Projects />}
-      {page === "contact" && <Contact />}
-      <Footer />
-    </div>
+    <DarkModeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+      <div
+        className={`w-screen h-full p-5 pb-4 font-wendy dark:bg-dark text-dark bg-light dark:text-light ${
+          isSidebar ? "h-screen overflow-hidden" : ""
+        }`}
+      >
+        <Header
+          isSidebar={isSidebar}
+          showSidebar={showSidebar}
+          hideSidebar={hideSidebar}
+          // setPage={setPage}
+        />
+        {isSidebar && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-80"
+            onClick={hideSidebar}
+          ></div>
+        )}
+        {page === "home" && <Home />}
+        {page === "about" && <About />}
+        {page === "projects" && <Projects />}
+        {page === "contact" && <Contact />}
+        <Footer />
+      </div>
+    </DarkModeContext.Provider>
   );
 }
 
--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,30 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faToggleOn, faToggleOff } from "@fortawesome/free-solid-svg-icons";
+import DarkModeContext from "./DarkModeContext";
 
 const DarkModeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState();
+  const { isDarkMode, setIsDarkMode } = useContext(DarkModeContext);
 
   const toggleDarkMode = () => {
     document.documentElement.classList.toggle("dark");
     setIsDarkMode((preVal) => !preVal);
   };
-
-  // Ensure dark mode is applied on initial load if the user has it enabled in their system settings
-  useEffect(() => {
-    setIsDarkMode(() => {
-      const darkMode =
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-      if (darkMode) {
-        document.documentElement.classList.add("dark");
-        return true;
-      }
-
-      return false;
-    });
-  }, []);
 
   return (
     <div className="flex items-center gap-x-1.5 text-[11px]">
