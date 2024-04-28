83c6f0e6d86433290419a16904aa1ab9ec1f8d6c
Archit Kakkar <education.archit@gmail.com>
2024-04-29 02:24:44 +0530

    remove nested <p> to eliminate console warning.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -12,18 +12,20 @@ const Homepage = () => {
           Web
         </span>
       </h4>
-      <p className="px-4 py-4 text-sm font-bold sm:text-base 3xl:text-lg dark:font-normal border-3 border-dark dark:border-light sm:p-5 rounded-3xl font-abeezee md:w-10/12 2xl:w-3/4">
-        I'm a software engineer specializing in building (and occasionally
-        designing) exceptional digital experiences. Currently, I'm focused on
-        building accessible, human-centered SAAS applications. I strive to
-        create solutions that are not only functional but also intuitive and
-        user-friendly.
+      <div className="px-4 py-4 text-sm font-bold sm:text-base 3xl:text-lg dark:font-normal border-3 border-dark dark:border-light sm:p-5 rounded-3xl font-abeezee md:w-10/12 2xl:w-3/4">
+        <p>
+          I'm a software engineer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I'm focused on
+          building accessible, human-centered SAAS applications. I strive to
+          create solutions that are not only functional but also intuitive and
+          user-friendly.
+        </p>
         <p className="pt-3">
           If you're interested in learning more about my background, projects,
           or how I can contribute to your team, feel free to check out my
           resume.
         </p>
-      </p>
+      </div>
       <button className="dark:bg-light bg-dark mt-6 md:mt-8 py-2 px-5 sm:px-7 sm:py-2.5 rounded-lg tracking-wide hover:opacity-90 hover:dark:opacity-60 lg:text-lg 3xl:text-xl lg:py-2 3xl:py-3 lg:px-6 3xl:px-8">
         <a
           href="https://drive.google.com/file/d/1Oo7g3iWFiLirqHAW1kEpk2D_XP125Tzg/view"
