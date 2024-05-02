fadea046c44da8a45a652ef27d20422bc334af93
Archit Kakkar <education.archit@gmail.com>
2024-05-02 13:55:37 +0530

    Add /contact to cors origin.

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://architkakkar.vercel.app"],
+    origin: ["https://architkakkar.vercel.app/contact"],
     methods: ["POST", "GET"],
     credentials: true,
   })
